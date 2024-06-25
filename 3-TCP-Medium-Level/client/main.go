36a4b04e9cc55f4b4463947800a041728b182750
Sadık Sünbül <109942759+SadikSunbul@users.noreply.github.com>
2024-06-25 15:34:54 +0300

    yorumlar eklendi

--- 3-TCP-Medium-Level/client/main.go
+++ 3-TCP-Medium-Level/client/main.go
@@ -8,30 +8,30 @@ import (
 
 func main() {
 
-	conn, err := net.Dial("tcp4", ":3000")
+	conn, err := net.Dial("tcp4", ":3000") // server listen port 3000 olarak ayarlandı
 	if err != nil {
 		panic(err)
 	}
 
-	defer conn.Close()
+	defer conn.Close() // server bağlantısı kapatıldı
 
-	go func() {
+	go func() { //1 DEN FAZLA client ustunden hızmet verebılmek ıcın yaptık
 		for {
-			buff := make([]byte, 8)
-			_, err := conn.Read(buff[:])
-			_, _, msg := readMessage(buff)
+			buff := make([]byte, 8)        // buffer hazır
+			_, err := conn.Read(buff[:])   // okuma yapıldı
+			_, _, msg := readMessage(buff) // mesaj okundu
 			if err != nil {
 				fmt.Println("read error:", err)
-				conn.Close()
+				conn.Close() // client bağlantısı kapatıldı
 				break
 			}
-			fmt.Printf("mssage from server : %s \n", msg)
+			fmt.Printf("mssage from server : %s \n", msg) // mesaj yazdırıldı
 		}
 	}()
 
 	for i := 0; i < 10; i++ {
-		data := createMessage(MessageTypeText, "hello from client")
-		_, err = conn.Write(data)
+		data := createMessage(MessageTypeText, "hello from client") // mesaj olusturuldu
+		_, err = conn.Write(data)                                   // mesaj yazıldı
 		if err != nil {
 			fmt.Println("write error:", err)
 		}
@@ -55,17 +55,19 @@ uint32  | uint32  | string
 
 	type	 | length  | data
 */
+// mesaj olusturuldu
 func createMessage(mtype int, data string) []byte {
-	buf := make([]byte, 4+4+len(data))
+	buf := make([]byte, 4+4+len(data))                        // buffer olusturuldu
 	binary.LittleEndian.PutUint32(buf[0:], uint32(mtype))     // 0 dan ıtıbaren
 	binary.LittleEndian.PutUint32(buf[4:], uint32(len(data))) //4 en ıtıbaren
 	copy(buf[8:], []byte(data))                               //8 den ıtıbaren
 	return buf
 }
 
+// mesaj okundu
 func readMessage(data []byte) (mtype, mlen uint32, msg string) {
-	mtype = binary.LittleEndian.Uint32(data[0:])
-	mlen = binary.LittleEndian.Uint32(data[4:])
-	msg = string(data[8:])
+	mtype = binary.LittleEndian.Uint32(data[0:]) // 0 dan ıtıbaren
+	mlen = binary.LittleEndian.Uint32(data[4:])  //4 en ıtıbaren
+	msg = string(data[8:])                       // 8 den ıtıbaren
 	return mtype, mlen, msg
 }
--- 3-TCP-Medium-Level/server/main.go
+++ 3-TCP-Medium-Level/server/main.go
@@ -7,36 +7,37 @@ import (
 )
 
 func main() {
-	ls, err := net.Listen("tcp4", ":3000")
+	ls, err := net.Listen("tcp4", ":3000") // server listen port 3000 olarak ayarlandı
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("connection ready!")
-	for { //1 DEN FAZLA client ustunden hızmet verebılmek ıcın yaptık
-		conn, err := ls.Accept()
+	fmt.Println("connection ready!") // server bağlantısı hazır
+	for {                            //1 DEN FAZLA client ustunden hızmet verebılmek ıcın yaptık
+		conn, err := ls.Accept() // server bağlantısını kabul etti
 		if err != nil {
 			fmt.Print("connetion error:", err)
 			continue
 		}
-		handler(conn)
+		handler(conn) // client bağlantısı kabul edildi
 	}
 }
 
+// client bağlantısı kabul edildi
 func handler(conn net.Conn) {
-	fmt.Println("connection accepted:", conn.RemoteAddr().String())
+	fmt.Println("connection accepted:", conn.RemoteAddr().String()) // client bağlantısı kabul edildi
 
-	for {
-		header := make([]byte, 8)
-		_, err := conn.Read(header[:])
+	for { //1 DEN FAZLA client ustunden hızmet verebılmek ıcın yaptık
+		header := make([]byte, 8)      // header hazır
+		_, err := conn.Read(header[:]) // okuma yapıldı
 		if err != nil {
 			fmt.Println("read error:", err)
-			conn.Close()
+			conn.Close() // client bağlantısı kapatıldı
 			break
 		}
-		mlen := binary.LittleEndian.Uint32(header[4:])
-		databuff := make([]byte, mlen)
-		conn.Read(databuff[:])
+		mlen := binary.LittleEndian.Uint32(header[4:]) // headerden uzunluk okundu
+		databuff := make([]byte, mlen)                 // buffer hazır
+		conn.Read(databuff[:])                         // okuma yapıldı
 		if err != nil {
 			fmt.Println("read error:", err)
 			conn.Close()
@@ -44,10 +45,10 @@ func handler(conn net.Conn) {
 		}
 
 		var messagebug []byte
-		messagebug = append(messagebug, header...)
-		messagebug = append(messagebug, databuff...)
-		mtype, _, msg := readMessage(messagebug)
-		fmt.Printf("type:%d, len:%d, message:%s \n", mtype, mlen, msg)
+		messagebug = append(messagebug, header...)                     // header eklendi
+		messagebug = append(messagebug, databuff...)                   // buffer eklendi
+		mtype, _, msg := readMessage(messagebug)                       // mesaj okundu
+		fmt.Printf("type:%d, len:%d, message:%s \n", mtype, mlen, msg) // mesaj yazdırıldı
 	}
 }
 
@@ -59,17 +60,19 @@ uint32  | uint32  | string
 
 	type	 | length  | data
 */
+// mesaj olusturuldu
 func createMessage(mtype int, data string) []byte {
-	buf := make([]byte, 4+4+len(data))
+	buf := make([]byte, 4+4+len(data))                        // buffer olusturuldu
 	binary.LittleEndian.PutUint32(buf[0:], uint32(mtype))     // 0 dan ıtıbaren
 	binary.LittleEndian.PutUint32(buf[4:], uint32(len(data))) //4 en ıtıbaren
 	copy(buf[8:], []byte(data))                               //8 den ıtıbaren
 	return buf
 }
 
+// mesaj okundu
 func readMessage(data []byte) (mtype, mlen uint32, msg string) {
-	mtype = binary.LittleEndian.Uint32(data[0:])
-	mlen = binary.LittleEndian.Uint32(data[4:])
-	msg = string(data[8:])
+	mtype = binary.LittleEndian.Uint32(data[0:]) // 0 dan ıtıbaren
+	mlen = binary.LittleEndian.Uint32(data[4:])  //4 en ıtıbaren
+	msg = string(data[8:])                       // 8 den ıtıbaren
 	return mtype, mlen, msg
 }
